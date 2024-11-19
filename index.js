028957c7c51c5eb35b044a392ec5be7cd3c97150
ArmandoReyes105 <arrey105@outlook.com>
2024-11-19 13:51:59 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const cors = require("cors")
+const dotenv = require('dotenv')
+const app = express()
+
+//Cargar configuración del archivo .env
+dotenv.config()
+
+//Requerido para entender los datos en JSON
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+//Se requiere si se accede desde un navegador web
+var corsOptions = {
+    origin: ["http://localhost:8080", "http://localhost:8081"],
+    methods: "GET,PUT,POST,DELETE",
+}
+
+app.use(cors(corsOptions))
+
+//Swagger
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger-output.json')
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+//Bitacora
+app.use(require("./middlewares/bitacora.middleware"))
+
+//Rutas
+app.use("/api/categorias", require('./routes/categorias.routes'))
+app.use("/api/productos", require('./routes/productos.routes'))
+app.use("/api/usuarios", require('./routes/usuarios.routes'))
+app.use("/api/roles", require('./routes/roles.routes'))
+app.use("/api/auth", require('./routes/auth.routes'))
+app.use("/api/archivos", require('./routes/archivos.routes'))
+app.use("/api/bitacora", require('./routes/bitacora.routes'))
+app.get('*', (req, res) => { req.status(404).send("Recurso no encontrado")})
+
+//Middleware para el manejo de errores
+const errorhandler = require('./middlewares/errorhandler.middleware')
+app.use(errorhandler)
+
+//Inicia el servidor en el puerto sever_port
+app.listen(process.env.SERVER_PORT, () => {
+    console.log(`Aplicación de ejemplo escuchando en el puerto ${process.env.SERVER_PORT}`)
+})
---
+++ services/jwttoken.service.js
@@ -0,0 +1,36 @@
+const jwt = require('jsonwebtoken')
+const jwtSecret = process.env.JWT_SECRET
+const ClaimTypes = require('../config/claimtypes')
+
+const GeneraToken = (email, nombre, rol) => {
+    //Utilizamos los nombres de claims estandar
+    const token = jwt.sign({
+        [ClaimTypes.Name]: email, 
+        [ClaimTypes.GivenName]: nombre,
+        [ClaimTypes.Role]: rol,
+        "iss": "ServidorArmando",
+        "aud": "ClientesArmando"
+    },
+        jwtSecret, {
+        expiresIn: '20m'
+    })
+    return token;
+}
+
+const TiempoRestanteToken = (req) => {
+    try {
+        const authHeader = req.header('Authorization')
+        const token = authHeader.split(' ')[1]
+        const decodedToken = jwt.verify(token, jwtSecret)
+
+        //Regresa el tiempo restante en minutos
+        const time = (decodedToken.exp - (new Date().getTime() / 1000))
+        const minutos = Math.floor(time / 60)
+        const segundos = Math.floor(time - minutos * 60)
+        return "00" + minutos.toString().padStart(2, "0") + ':' + segundos.toString().padStart(2, "0")
+    }catch(error){
+        return null
+    }
+}
+
+module.exports = { GeneraToken, TiempoRestanteToken}
